2e1bc4afcf113688cb9f8cf7b013ae39048ae039
Alberto <albertomadueno@gmail.com>
2017-01-08 16:45:58 -0800

    update

--- client/env/config.js
+++
@@ -1,4 +0,0 @@
-$.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader('X-Parse-Application-Id', '6UJYuifdHSHnOvG2DiYXU6cwluUvgDiVOpr8Weqi');
-  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'gYu7Z35zwiNz4BaNgwyaq9u9A36eVHd38MiDvCH5');
-});
